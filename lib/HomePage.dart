f7776be96298eb79c27aa0461644c0c6d7f4b97b
robson <robson@magario.com>
2020-06-21 14:59:51 +0800

    This version can catch the email address.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -16,7 +16,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _userId = "";
+  String _email = "";
 
+  @override
+  void initState() {
+    super.initState();
+    widget.auth.getCurrentUser().then((user) {
+      setState(() {
+        if (user != null) {
+          _userId = user?.uid;
+          _email = user?.email;
+        }
+      });
+    });
+  }
   _signOut() async {
     try {
       await widget.auth.signOut();
@@ -46,9 +60,26 @@ class _HomePageState extends State<HomePage> {
           
           child: Column (
             children: <Widget>[Row(
-              children: [Text('My Hong Kong address:')]
-            ),Row(
-                children: [Text('27A, The Met Focus'),
+              children: <Widget>[Text(_email)],
+            )
+              ,Row(
+              children: [Container(
+
+                  child:Text('My Hong Kong address:'))]
+            ),Flexible(
+              child: Container(
+
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+              children: <Widget>[Text('Use this address for receiving your packages in HK.')],
+            ),),),
+              Row(
+                children: <Widget>[Text(' ')],
+              ),
+              Row(
+                children: [Container(
+                  width: 200.0,
+                  child:Text('27A, The Met Focus'),),
                   RaisedButton(
                     child: Text('Copy to Clipboard'),
                     onPressed: () {
@@ -68,10 +99,93 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ]
             ),Row(
-                children: [Text('8 Pak Kung Street')]
+                children: [Container(
+                    width: 200.0,
+                    child: Text('Street: 8 Pak Kung Street'),),
+                  RaisedButton(
+                    child: Text('Copy to Clipboard'),
+                    onPressed: () {
+                      FlutterClipboardManager.copyToClipBoard(
+                          "8 Pak Kung Street")
+                          .then((result) {
+                        final snackBar = SnackBar(
+                          content: Text('Copied to Clipboard'),
+                          action: SnackBarAction(
+                            label: 'Okay',
+                            onPressed: () {},
+                          ),
+                        );
+                        Scaffold.of(context).showSnackBar(snackBar);
+                      });
+                    },
+                  ),]
             ),Row(
-                children: [Text('Hong Kong')]
-            ),],
+                children: [Container(
+                    width: 200.0,
+                    child: Text('District: Hung Hom'),),
+                  RaisedButton(
+                    child: Text('Copy to Clipboard'),
+                    onPressed: () {
+                      FlutterClipboardManager.copyToClipBoard(
+                          "Hung Hom")
+                          .then((result) {
+                        final snackBar = SnackBar(
+                          content: Text('Copied to Clipboard'),
+                          action: SnackBarAction(
+                            label: 'Okay',
+                            onPressed: () {},
+                          ),
+                        );
+                        Scaffold.of(context).showSnackBar(snackBar);
+                      });
+                    },
+                  ),]
+            ),
+              Row(
+                  children: [Container(
+                      width: 200.0,
+                      child: Text('City: Hong Kong'),),
+                    RaisedButton(
+                      child: Text('Copy to Clipboard'),
+                      onPressed: () {
+                        FlutterClipboardManager.copyToClipBoard(
+                            "Hong Kong")
+                            .then((result) {
+                          final snackBar = SnackBar(
+                            content: Text('Copied to Clipboard'),
+                            action: SnackBarAction(
+                              label: 'Okay',
+                              onPressed: () {},
+                            ),
+                          );
+                          Scaffold.of(context).showSnackBar(snackBar);
+                        });
+                      },
+                    ),]
+              ),
+              Row(
+                  children: [Container(
+                    width: 200.0,
+                    child: Text('TEL: 6818-3401'),),
+                    RaisedButton(
+                      child: Text('Copy to Clipboard'),
+                      onPressed: () {
+                        FlutterClipboardManager.copyToClipBoard(
+                            "6818-3401")
+                            .then((result) {
+                          final snackBar = SnackBar(
+                            content: Text('Copied to Clipboard'),
+                            action: SnackBarAction(
+                              label: 'Okay',
+                              onPressed: () {},
+                            ),
+                          );
+                          Scaffold.of(context).showSnackBar(snackBar);
+                        });
+                      },
+                    ),]
+              ),
+            ],
           )
         )
       ),
