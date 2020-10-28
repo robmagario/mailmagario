f56554ce13e80be0c8628dc2d3dfdb8b78e138dd
robson <robson@magario.com>
2020-10-28 21:53:40 +0800

    added icons

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,13 @@ import 'package:mailmagario/my_app.dart';
 import 'package:mailmagario/providers/login_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
