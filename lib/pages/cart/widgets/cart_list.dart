aa6310d0371bb7a5b0eede28d694d2f466a7a782
robson <robson@magario.com>
2021-02-07 16:48:01 +0800

    styles in shopping cart got a bit better

--- lib/pages/cart/widgets/cart_list.dart
+++ lib/pages/cart/widgets/cart_list.dart
@@ -5,11 +5,11 @@ import 'package:mailmagario/pages/cart/cart_riverpod.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mailmagario/pages/product/product_view.dart';
 import 'package:mailmagario/pages/create_order/create_order_view.dart';
-
+import 'package:timeago/timeago.dart' as timeago;
 
 
 class CartList extends ConsumerWidget {
-  CartList({Key key}) : super(key: key);
+ // CartList({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
@@ -18,19 +18,22 @@ class CartList extends ConsumerWidget {
     return Column(
       children: [
       Container(
-      height: 100,
+      height: 500,
       child: ListView.builder(
         itemCount: productList.length,
         itemBuilder: (BuildContext context, int index) {
           CartItemModel cartItem = productList[index];
           return ListTile(
             title: Text(productList[index].product.productName),
+            subtitle: Text('Date: ${timeago.format(productList[index].product.createdOn.toDate())} Tracking Number: ${productList[index].product.receivedTrackingNumber} Weight: ${productList[index].product.weight.toString()} grams'),
             trailing: IconButton(
               icon: Icon(Icons.delete),
               // 3
               onPressed: () =>
                   context.read(cartRiverpodProvider).remove(cartItem.id),
             ),
+            isThreeLine: true,
+
           );
         },
       ),
