0b8b852f1ad14df1bbd942f0ef652dca02e42e0b
robson <robson@magario.com>
2021-03-07 20:29:24 +0800

    not done yet but this is the first step to get rid of getX.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 import 'package:mailmagario/controllers/auth_controller.dart';
 import 'package:mailmagario/controller.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+/*
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -18,4 +18,81 @@ void main() async {
       child: MyApp(),
     ),
   );
+}
+
+ */
+
+import 'package:mailmagario/pages/auth/auth_providers.dart';
+import 'package:mailmagario/screens/home_screen.dart';
+import 'package:mailmagario/screens/login_page.dart';
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StartPage(),
+    );
+  }
+}
+
+class StartPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Something Went wrong"),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AuthChecker();
+        }
+        //loading
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class AuthChecker extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final _authState = watch(authStateProvider);
+    return _authState.when(
+      data: (value) {
+        if (value != null) {
+          return HomeScreen();
+        }
+        return LoginPage();
+      },
+      loading: () {
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+      error: (_, __) {
+        return Scaffold(
+          body: Center(
+            child: Text("OOPS"),
+          ),
+        );
+      },
+    );
+  }
 }
